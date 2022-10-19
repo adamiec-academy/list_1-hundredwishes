bf6b353c35c3abaf07f97efa1b410b82aef6537a
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-19 22:58:57 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,6 +1,6 @@
 def cross(n):
     for i in range(n*3):
         if (i<n) or ((2*n)<=i):    
-            print(' '*(n-1),'*'*n)
+            print(' '*(n-1),'*'*n,' '*n)
         else:
             print('*'*n*3)
