51051f68fdf912953967d11b9809b7781e85c8bb
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-26 18:47:19 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,6 +1,6 @@
 def chess_board(n,k):
     for i in range(n):
         for j in range(k):
-            print((' '*k+'#'*k)*k)
+            print((' '*k+'#'*k)*n)
         for j in range(k):
-            print(('#'*k+' '*k)*k)
+            print(('#'*k+' '*k)*n)
