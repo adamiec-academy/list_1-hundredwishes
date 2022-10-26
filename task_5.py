ced2889202cbbd9ff044f623921549a44c6d9cb6
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-26 19:38:29 +0200

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,14 @@
 def factorial(n):
-    pass
-
+    if n==0:
+        return 1
+    else:
+        a=1
+        for i in range(1,n):
+            a=a*(i+1)
+    return a
 
 def report():
-    pass
+    digit=1
+    for i in range(101):
+        print(f"{i}! is {len(str(digit))} digits long")
+        digit=digit*(i+1)
